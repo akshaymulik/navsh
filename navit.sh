47ab6b96b798b5581aa824f5a1a136f462c2647e
Akshay Sanjay Mulik <akshaymulik399@gmail.com>
2018-02-28 01:56:37 +0530

    Update navit.sh
    Install GPS and BlueMan

--- navit.sh
+++ navit.sh
@@ -4,6 +4,7 @@ sudo apt-get install gcc g++ cmake make zlib1g-dev libpng12-dev librsvg2-bin
 sudo apt-get install libsdl-image1.2-dev libdevil-dev libglc-dev freeglut3-dev libxmu-dev libfribidi-dev
 sudo apt-get install libglc-dev freeglut3-dev libgl1-mesa-dev libfreeimage-dev
 sudo apt-get install libqt4-dev libgps-dev espeak
+sudo apt-get install gpsd gpsd-clients blueman
 git clone -b trunk --single-branch https://github.com/navit-gps/navit.git
 mkdir navit-build
 cd navit-build
